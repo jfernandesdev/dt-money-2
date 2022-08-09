4b1fad895fcd3433d0ab6aadfb360607d50fbbc1
jfernandesdev <jfernandes.dev@gmail.com>
2022-08-09 14:27:05 -0300

    STYLE: transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,45 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
-import { Container } from './styles'
+import { 
+  TransactionsContainer, 
+  TransactionsTable, 
+  PriceHighlight 
+} from './styles'
 
 export function Transactions() {
   return (
-    <Container>
+    <>
       <Header />
       <Summary />
-    </Container>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$  12.000
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>09/08/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  -R$  59.00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>09/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
   )
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,38 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme['gray-700']};
+  
+    &:first-child {
+      border-radius:  6px 0 0 6px;
+    }
+
+    &:last-child {
+      border-radius: 0 6px 6px 0;
+    }
+  }
+`
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome';
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${(props) => props.variant === 'income' ? 
+    props.theme['green-300'] : 
+    props.theme['red-300']
+  };
+`

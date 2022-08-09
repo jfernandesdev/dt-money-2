0af5ec55d0ce7e7db4be2275e603e890bbf04de3
jfernandesdev <jfernandes.dev@gmail.com>
2022-08-09 11:04:42 -0300

    CHORE: project setup with Vite, Typescript and Styled Components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}

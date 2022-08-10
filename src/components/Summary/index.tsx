e3f6d71a349dfe2788e9c7c6fa09d67003c8bfbe
jfernandesdev <jfernandes.dev@gmail.com>
2022-08-10 11:38:27 -0300

    FEAT: fomatting of values prices and dates

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,6 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
 import { useTransaction } from '../../hooks/useTransactions'
+import { priceFormatter } from '../../utils/formatter'
 import { SummaryContainer, SummaryCard } from './styles'
 
 export function Summary() {
@@ -32,7 +33,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -41,7 +42,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -50,7 +51,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#FFF" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import { SearchForm } from '../../components/SearchForm'
 import { Summary } from '../../components/Summary'
 
 import { useTransaction } from '../../hooks/useTransactions'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 import {
   TransactionsContainer,
@@ -27,11 +28,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>

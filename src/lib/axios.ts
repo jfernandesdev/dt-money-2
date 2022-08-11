dd7ef38c2752688d6779f79f6b8ce56b00fca730
Jeferson Fernandes <jfernandes.dev@gmail.com>
2022-08-11 19:20:15 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/jfernandesdev/dt-money-2',
 })
